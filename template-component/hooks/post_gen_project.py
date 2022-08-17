43aea2c7d2dfba8743bb3ad86f0ae679fa01f6e1
thoaitrands <thoaitrands@gmail.com>
2022-08-17 22:55:19 +0700

    add hook to test

--- template-component/hooks/post_gen_project.py
+++ template-component/hooks/post_gen_project.py
@@ -1,1 +1,16 @@
 print("THIS IS A HOOKS")
+
+import os
+import shutil
+
+print(os.getcwd())  # prints /absolute/path/to/{{cookiecutter.project_slug}}
+
+def remove(filepath):
+    print("filepath = %s", filepath)
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+    elif os.path.isdir(filepath):
+        shutil.rmtree(filepath)
+
+if "{{ cookiecutter.enable_windows }}".lower() == "n":
+    remove(os.path.join("{{cookiecutter.directory_name}}", "windows_configs"))
